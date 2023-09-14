7bf16b51d2ea7ce2f249538e176b9c5b0fcd5c3d
Alexey Khlyzov <alex.khlizov@gmail.com>
2023-09-14 16:07:18 +0300

    contextLoads test

---
+++ src/test/java/com/musala/interview/DronesDispatcherApplicationTest.java
@@ -0,0 +1,10 @@
+package com.musala.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesDispatcherApplicationTest {
+    @Test
+    void contextLoads() {}
+}
